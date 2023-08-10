2ba25126696eee016eeb5e728b5198a975a55f5a
Animesh Kalita <104593018+Ak-nut-47@users.noreply.github.com>
2023-08-10 19:49:01 +0530

    cors external

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
-
+const cors = require("cors")
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
-
+server.use(cors())
 server.use(middlewares);
 server.use(router);
-server.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin",
-        "*");
-    res.header("Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type,  Accept")
-    next()
-});
+
 
 server.listen(port);
